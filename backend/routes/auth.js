91be2171c90aea0c4ac61ad4cb25c193487ea231
Vandana B Gowda <vandanabgowda@Vandana-ka-MacBook-Pro.local>
2024-10-03 01:24:09 +0530

    Switch from bcrypt to bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const Form = require('../models/Form');
